00b36d28858f1d93984f1d4afa3e154c7d81ee5e
n8bar <nate@nateTheProgrammer.com>
2025-11-06 17:20:09 -0700

    fix(403): align copy with test expectation

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,3 +1,7 @@
+@php
+    $details = isset($details) && is_string($details) && trim($details) !== '' ? $details : null;
+@endphp
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -9,7 +13,10 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 space-y-4">
-                    <p>{{ __("Sorry, you don't have permission to view this page.") }}</p>
+                    <p>{{ __("Sorry, you don't have permission.") }}</p>
+                    @if ($details)
+                        <p class="text-gray-600">{{ $details }}</p>
+                    @endif
                     <p>
                         <a href="{{ route('dashboard') }}" class="text-blue-600 underline">
                             &larr; {{ __('Back to dashboard') }}
